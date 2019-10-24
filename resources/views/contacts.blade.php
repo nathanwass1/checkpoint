23ef3ad5c6ff4966c5ffb0dd549691a080dc2613
nathanwass1 <nathanwass@live.co.uk>
2019-10-24 11:13:46 +0000

    first commit

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+@extends('layout')
+
+@section('style')
+@endsection
+</head>
+
+<body>
+
+@section('content')
+@endsection
+
+<div class="content">
+<h1> Contacts: <h1>
+<p>
+Nathan Wass<br>
+Email: <br>N.Wass2784@student.leedsbeckett.ac.uk
+</p>
+</div>
+
+
+
+
+</body>
+
+</html>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>@yield('title', 'Film Site ')</title>
+<link rel ="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+@yield('style') 
+
+     <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+            
+        </style>
+@yield('style')
+</head>
+<body>
+<div class="container">
+@yield('content')
+</div>
+<div class="content">
+    <div class="links">
+        <li><a href="/">Home Page</a></li>
+        <li><a href="/contacts">Contacts Page</a></li>
+        <li><a href="/films">Films Page</a></li>
+    </div>
+</div>
+@yield('Content')
+</body>
+</html>
